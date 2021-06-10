d99e8da4e2c577bbb704de4b967f725a24559939
Kristian-ss <kristian_aprianto_28rpl@student.smktelkom-mlg.sch.id>
2021-06-10 13:06:12 +0700

    Modul6

---
+++ app/Buku.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Buku extends Model
+{
+    protected $table = 'buku';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = ['judul', 'penulis', 'penerbit', 'tahun'];
+}
